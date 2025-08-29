e30de0b75f566ec77433ec02b7782c6de2bb6b7a
Lisha Maharjan <lishamaharjan358@gmail.com>
2025-08-29 21:23:47 +0000

    Set up tests for the models and documented the models.

---
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -0,0 +1,9 @@
+select 
+    id as payment_id,
+    orderid as order_id,
+    paymentmethod as payment_method,
+    status,
+    amount / 100 as amount, -- amount stored in cents, convert to dollars
+    created as created_at
+    
+from {{ source('stripe', 'payment') }}
---
+++ tests/assert_stg_payments_amount_is_positive.sql
@@ -0,0 +1,15 @@
+with payments as (
+    select * 
+    from {{ ref("stg_stripe__payments") }}
+)
+
+
+select 
+    order_id,
+    sum(amount) as total_amount
+from
+    stg_stripe__payments
+group by
+    order_id
+having
+    total_amount < 0
